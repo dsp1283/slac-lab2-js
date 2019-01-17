fa80ec5a14c500a85c0dd30143cc744b918742a0
Samuel Wanck <samuelwanck@gmail.com>
2019-01-17 14:48:45 -0800

    Adding instructions

--- AddNumbers.js
+++ AddNumbers.js
@@ -7,6 +7,7 @@
                         var val2 = parseInt(document.getElementById("value2").value);
                         var ansD = document.getElementById("answer");
                         ansD.value = val1 + val2;
+			print("Sam was here!!!");
                 }
         </script>
   </head>
